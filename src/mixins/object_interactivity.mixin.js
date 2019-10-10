b4c813a36274757d24fd254ee58627b740720417
Lukas Gjetting <lukasgjetting@gmail.com>
2019-10-10 09:18:07 +0200

    Draw delete icon (as a Path2D) instead of tl control

--- src/mixins/object_interactivity.mixin.js
+++ src/mixins/object_interactivity.mixin.js
@@ -251,9 +251,9 @@
       this._setLineDash(ctx, styleOverride.cornerDashArray || this.cornerDashArray, null);
 
       // top-left
-      this._drawControl('tl', ctx, methodName,
+      this._drawDeleteControl('tl', ctx, icon,
         left,
-        top, styleOverride);
+        top);
 
       // top-right
       this._drawControl('tr', ctx, methodName,
@@ -330,6 +330,16 @@
             ctx.strokeRect(left, top, size, size);
           }
       }
+    },
+
+    _drawDeleteControl: function(control, ctx, left, top, icon) {
+      if (!this.isControlVisible(control)) {
+        return;
+      }
+
+      var path = new Path2D('M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z');
+
+      ctx.stroke(path, left, top, this.cornerSize, this.cornerSize);
     },
 
     /**
